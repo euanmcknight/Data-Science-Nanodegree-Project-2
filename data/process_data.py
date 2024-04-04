5cc10567c792da7657db9023c0b0bcab2fa1f86d
Euan McKnight <euan.mcknight@googlemail.com>
2024-04-04 08:45:29 +0100

    Update process_data.py
    Converts values to binary

--- data/process_data.py
+++ data/process_data.py
@@ -43,6 +43,8 @@ def clean_data(df):
     df.drop('categories', axis=1, inplace=True)
     # Concatenate the original dataframe with the new `categories` dataframe
     df = pd.concat([df, categories], axis= 1)
+    # Drop rows where 'related' column equals 2
+    df = df[df['related'] != 2]
     # Drop duplicates
     df = df.drop_duplicates()
     return df
